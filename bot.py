3f7903f041518f8c9a3b67e09d2b224281c535d0
MHM.Iq <lifemahdi1@gmail.com>
2026-09-15 00:19:33 +0000

    Add automatic dummy server for Render port

--- bot.py
+++ bot.py
@@ -1,3 +1,19 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import threading
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running 24/7!")
+
+def run_dummy_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), SimpleHandler)
+    server.serve_forever()
+
+threading.Thread(target=run_dummy_server, daemon=True).start()
+
 import os
 import glob
 import logging
